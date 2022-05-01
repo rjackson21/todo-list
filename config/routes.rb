79616c116a0ec45f31963c159717e0e742e57f68
Regina Jackson <rjackson156@gmail.com>
2022-05-01 17:36:59 -0400

    updated routes to reflect new controller name

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get 'todo/index'
+  post 'todo/index'
+  post 'todo/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "todo#index"
 
-  resources :todo 
+  resources :todos
   # Defines the root path route ("/")
   # root "articles#index"
 end
