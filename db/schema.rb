a4b637f3b38e85b0b020fa0b94e93954b180d8b7
Regina Jackson <rjackson156@gmail.com>
2022-05-01 18:30:24 -0400

    remove complete field from todos table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_28_175525) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_01_222654) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "todos", force: :cascade do |t|
     t.string "content"
-    t.boolean "complete"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
